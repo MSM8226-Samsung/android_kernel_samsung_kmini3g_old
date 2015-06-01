aacd9437c01057e1b2fa3fb5a17293caacc6bd74
Quarx2k <agent00791@gmail.com>
2015-06-01 09:29:22 -0400

    Add IMX219 Sensor
    Change-Id: I425366e869522157c62e6ffed98be7484b8b08a3
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- arch/arm/mach-msm/clock-8226.c
+++ arch/arm/mach-msm/clock-8226.c
@@ -3470,11 +3470,17 @@ static struct clk_lookup msm_clocks_8226[] = {
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "21.qcom,camera"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "22.qcom,camera"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "0.qcom,camera"),
+#ifdef CONFIG_IMX219
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "20.qcom,camera"),
+#endif
 #if	defined(CONFIG_SEC_MATISSE_PROJECT) || defined (CONFIG_SEC_BERLUTI_PROJECT) || defined (CONFIG_SEC_ATLANTIC_PROJECT) ||\
 	defined (CONFIG_SEC_MEGA2_PROJECT) || defined(CONFIG_SEC_VASTA_PROJECT)
 	CLK_LOOKUP("cam_src_clk", mclk1_clk_src.c, "2.qcom,camera"),
 #else
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "2.qcom,camera"),
+#endif
+#ifdef CONFIG_IMX219
+	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "20.qcom,camera"),
 #endif
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "spi0.0"),
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "5a.qcom,camera"),
